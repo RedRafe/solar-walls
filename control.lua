d24b194b015e54b52716951969d29af8d0f04878
RedRafe <gabri97.gt+redrafe@gmail.com>
2021-11-29 14:40:00 +0100

    Refactoring control script

--- control.lua
+++ control.lua
@@ -1,66 +1,44 @@
-local function on_built( entity )
-  local pole = entity.surface.create_entity{name="wall-solar-panel", position=entity.position, force=entity.force}
-  pole.destructible = true
-  return true
+local SWControl = {}
+
+SWControl.interface = {
+  ["solar-wall"] = "wall-solar-panel"
+}
+
+SWControl.isKey = function(entity)
+  if SWControl.interface[entity.name] ~= nil then return true end
+  return false
 end
 
-script.on_event(defines.events.on_built_entity, function(event)
-  if event.created_entity.name == "solar-wall-2" then
-    on_built( event.created_entity )
-  end	
-end)
-
-script.on_event(defines.events.on_robot_built_entity, function(event)
-  if event.created_entity.name == "solar-wall-2" then
-	  on_built( event.created_entity )
-	end
-end)
-
-
-
-script.on_event(defines.events.script_raised_revive, function(event)
+local function on_built(event)
   local entity = event.entity or event.created_entity
-  if entity.name == "solar-wall-2" then
-    on_built(entity)
+  if SWControl.isKey(entity) then
+    local pole = entity.surface.create_entity
+    { 
+      name = SWControl.interface[entity.name],
+      position = entity.position,
+      force = entity.force
+    }
+    pole.destructible = false
   end
-end)
+end
 
-script.on_event(defines.events.script_raised_built, function(event)
-  local entity = event.entity or event.created_entity
-  if entity.name == "solar-wall-2" then
-    on_built(entity)
-  end
-end)
-
-script.on_event(defines.events.on_player_mined_entity, function(event)
-  if event.entity.name == "solar-wall-2" then
+local function on_destroy(event)
+  if SWControl.isKey(event.entity) then
     center = event.entity.position
     for _, entity in pairs(event.entity.surface.find_entities_filtered{
       area = {{center.x-0.5, center.y-0.5}, {center.x+0.5, center.y+0.5}},
-      name = "wall-solar-panel"}) do
-        entity.destroy()
-    end
-  end	
-end)
-
-script.on_event(defines.events.on_robot_mined_entity, function(event)
-  if event.entity.name == "solar-wall-2" then
-    center = event.entity.position
-    for _, entity in pairs(event.entity.surface.find_entities_filtered{
-      area = {{center.x-0.5, center.y-0.5}, {center.x+0.5, center.y+0.5}},
-      name = "wall-solar-panel"}) do
-      entity.destroy()
-    end
-  end
-end)
-
-script.on_event(defines.events.on_entity_died, function(event)
-  if event.entity.name == "solar-wall-2" then
-    center = event.entity.position
-    for _, entity in pairs(event.entity.surface.find_entities_filtered{
-      area = {{center.x-0.5, center.y-0.5}, {center.x+0.5, center.y+0.5}},
-      name = "wall-solar-panel"}) do
+      name = SWControl.interface[event.entity.name]}) do
       entity.destroy()
     end
   end	
-end)
+end
+
+-- build events
+script.on_event(defines.events.on_built_entity, on_built)
+script.on_event(defines.events.on_robot_built_entity, on_built)
+script.on_event(defines.events.script_raised_revive, on_built)
+script.on_event(defines.events.script_raised_built, on_built)
+-- destroy events
+script.on_event(defines.events.on_player_mined_entity, on_destroy)
+script.on_event(defines.events.on_robot_mined_entity, on_destroy)
+script.on_event(defines.events.on_entity_died, on_destroy)
